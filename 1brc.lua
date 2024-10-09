d4665a21ae773ddf8d1a68dffc043e3312e47ee2
Felipe Flores <felipeguilhermefs@gmail.com>
2024-10-09 10:13:17 +0200

    First take on 1brc in lua

---
+++ 1brc.lua
@@ -0,0 +1,62 @@
+local function read(filename)
+	local file = assert(io.open(filename, "r"))
+	local content = file:read("*all")
+	file:close()
+	return content
+end
+
+local MIN = 1
+local MAX = 2
+local SUM = 3
+local COUNT = 4
+
+local function accumulate(content)
+	local records = {}
+	for city, measurement in content:gmatch("(%S+);(%S+)") do
+		local temperature = tonumber(measurement)
+		local record = records[city]
+		if record then
+			if record[MIN] > temperature then
+				record[MIN] = temperature
+			end
+			if record[MAX] < temperature then
+				record[MAX] = temperature
+			end
+			record[SUM] = record[SUM] + temperature
+			record[COUNT] = record[COUNT] + 1
+		else
+			records[city] = { [MIN] = temperature, [MAX] = temperature, [SUM] = temperature, [COUNT] = 1 }
+		end
+	end
+	return records
+end
+
+local function join(statistics)
+	local result = {}
+	for city, stats in pairs(statistics) do
+		result[city] = stats[MIN] .. "/" .. (stats[SUM] / stats[COUNT]) .. "/" .. stats[MAX]
+	end
+	return result
+end
+
+local function formatJavaMap(tbl)
+	-- Results are in Java "Map.toString" format
+	local result = "{"
+	for k, v in pairs(tbl) do
+		result = result .. k .. "=" .. v .. ","
+	end
+	-- Remove leading commas from the result
+	if result ~= "" then
+		result = result:sub(1, result:len() - 1)
+	end
+	return result .. "}"
+end
+
+local function brc(filename)
+	local content = read(filename)
+	local statistics = accumulate(content)
+	local result = join(statistics)
+	print(formatJavaMap(result))
+end
+
+brc("small.csv")
